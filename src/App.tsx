dc361f9e33fb27d59d82e7f342dc1c6edc66ad77
Fabricio Mendonca <fabriciomendonca@gmail.com>
2022-11-18 15:22:24 +0100

    fix: checkout another new branch (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code for testing a fix branch
+          Another code for testing a fix branch
         </p>
         <a
           className="App-link"
