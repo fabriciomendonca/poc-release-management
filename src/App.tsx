54206f1c454c460b2b12b68ce08efb89e9130513
Fabricio Mendonca <fabriciomendonca@gmail.com>
2022-11-25 14:21:09 +0100

    feat!: this is a breaking change (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm just updating the text.
+          Adding a breaking change.
         </p>
         <a
           className="App-link"
