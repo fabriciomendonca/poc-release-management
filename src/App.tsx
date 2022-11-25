56bb53717220a4c1cfa52d65485d275ef0e6b590
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-25 10:57:59 +0100

    feat: pushing new commits to a pr

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Breaking change on top of breaking change
+          It shouldn't run the PR name verification if I push new commits
         </p>
         <a
           className="App-link"
