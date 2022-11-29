b6ce12c2c4303f8f79f6109ad7064983f57a4961
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-29 12:33:34 +0100

    fix: failing test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github
+          Learn React
         </a>
       </header>
     </div>
