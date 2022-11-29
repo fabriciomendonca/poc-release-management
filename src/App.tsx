17ad635b18017dc544f8b09bd846b7007da394ae
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-29 11:57:59 +0100

    chore: test package json update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          One more breaking change.
+          Package json must be updated!
         </p>
         <a
           className="App-link"
