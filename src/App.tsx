4953f4f672a986f3adb52750d46de3d21e8ae2ba
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-30 18:19:42 +0100

    chore: why is it not working?

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          With package.json update!!! 💪
+          With package.json update?
         </p>
         <a
           className="App-link"
