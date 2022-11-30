abc0cd6c4c94eadb66cd9d6fc029b4cdf52c5f5d
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-30 18:08:59 +0100

    feat: with package.json update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Automerging a PR?
+          With package.json update!!! 💪
         </p>
         <a
           className="App-link"
