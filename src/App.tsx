39d132905f961e2daa5e33c067a80afe96d6f7d3
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-12-01 14:22:18 +0100

    feat: keep PR up-to-date with releases

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          With package.json update?
+          With package.json update in the open PR
         </p>
         <a
           className="App-link"
