8d379794a7f55313bbd1cd6d9edc9fe17b37c012
Fabricio Mendonca <fabriciomendonca@gmail.com>
2022-12-22 15:14:51 +0100

    fix: some component fix (#88)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Skip release notes.
+          Some component fix.
         </p>
         <a
           className="App-link"
