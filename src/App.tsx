8c5a98c515924b3dd45ab953966df568714ba49e
Fabricio Mendonca <fabriciomendonca@gmail.com>
2023-04-19 11:37:34 +0200

    fix: Fixing some stuff after a breaking change (#101)
    * fix: Fixing some stuff after a breaking change
    
    * test: fix broken test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Introducing a breaking change.</p>
+        <p>Fixing some stuff after a breaking change.</p>
       </header>
     </div>
   );
